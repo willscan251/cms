787bdbc858734359e917e431c2865f4ab4d0c078
willscan251 <143242840+willscan251@users.noreply.github.com>
2025-08-28 22:51:40 -0500

    Add files via upload

--- scanland-cms-server.js
+++ scanland-cms-server.js
@@ -25,12 +25,12 @@ app.use((req, res, next) => {
         'https://www.scanlandconsulting.com',
         'https://thescanlandgroup.com',
         'https://www.thescanlandgroup.com',
-        'http://localhost:5500',
-        'http://127.0.0.1:5500',
-        'http://localhost:5501',
-        'http://127.0.0.1:5501',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000'
+        'localhost:5500',
+        '127.0.0.1:5500',
+        'localhost:5501',
+        '127.0.0.1:5501',
+        'localhost:3000',
+        '127.0.0.1:3000'
     ];
     
     const origin = req.headers.origin;
